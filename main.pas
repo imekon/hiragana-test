04b9629f3fc8ad1828638c6157adaaa7bbc694ba
Pete Goodwin <github@imekon.org>
2024-09-28 17:47:05 +0100

    added random piccie

--- main.pas
+++ main.pas
@@ -5,20 +5,25 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, item,
-  itemcat;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, ExtCtrls,
+  StdCtrls, item, itemcat;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
+    NextButton: TButton;
+    LetterImage: TImage;
     StatusBar: TStatusBar;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure OnNext(Sender: TObject);
   private
     m_itemCat: TItemCat;
     m_usable: TItems;
+
+    procedure PickAndDisplay;
   public
 
   end;
@@ -34,6 +39,7 @@ implementation
 
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
+  Randomize;
   m_itemCat := TItemCat.Create;
   m_itemCat.Load('database.json');
   m_usable := m_itemCat.GetUsable;
@@ -44,6 +50,21 @@ end;
 procedure TMainForm.FormDestroy(Sender: TObject);
 begin
   m_itemCat.Free;
+end;
+
+procedure TMainForm.OnNext(Sender: TObject);
+begin
+  PickAndDisplay;
+end;
+
+procedure TMainForm.PickAndDisplay;
+var
+  i, n: integer;
+
+begin
+  n := m_usable.Count;
+  i := Random(n);
+  LetterImage.Picture := m_usable[i].Image;
 end;
 
 end.
